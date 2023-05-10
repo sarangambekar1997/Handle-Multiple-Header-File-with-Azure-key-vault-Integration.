9c4df09f653e352c8c06123df7809a1f43cae38a
Sarang Ambekar <101887929+sarangambekar1997@users.noreply.github.com>
2023-05-10 10:43:10 +0530

    __init__.py

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ import main
 
 mainObj=main.Transformations()
 
-mainObj.separate_headers("Top100_DSR_themlc_Spotify_basic-desktop-bundle-free-premium_2021-01-01--2021-01-31_US_1of1_20210210T103521.tsv")
+mainObj.separate_headers("{FileName}")
